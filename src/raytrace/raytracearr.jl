37121c32e21b70eca1c596e112d84642e90aa73e
Zenna Tavares <zennatavares@gmail.com>
2017-11-13 05:24:28 -0500

    update to use broadcasting

--- src/raytrace/raytracearr.jl
+++ src/raytrace/raytracearr.jl
@@ -49,6 +49,50 @@ function rayintersect_arr(vec3size, scalarsize)
   rayint
 end
 
+"Ray Sphere Intersection"
+function rayintersect_arr_bcast()
+  rayint = CompArrow(:raysphere, [:rdir, :rorig, :scenter, :sradius],
+                                 [:doesintersect, :t0, :t1])
+  dotarr = dot_arr()
+  rdir, rorig, scenter, sradius, doesintersect, t0◂, t1◂ = ⬨(rayint)
+  # Constants
+  zerosscalarsarr = add_sub_arr!(rayint, source(0.0))
+  falsessarr = add_sub_arr!(rayint, source(false))
+  truessarr = add_sub_arr!(rayint, source(true))
+  zerosscalar = ◃(zerosscalarsarr, 1)
+  falses = ◃(falsessarr, 1)
+  trues = ◃(truessarr, 1)
+
+  l = bcast(scenter) - rorig    # [batch_size, width * height, 3]
+  tca = dotarr(l, rdir)             # [batch_size, width * height, 1]
+  radius2 = sradius * sradius       # [batch_size, 1]
+  d2 = dotarr(l, l) - tca * tca     # [batch_size, width * height, 1]
+  cond1 = tca < bcast(zerosscalar) # [batch_size, width * height, 1]
+  cond2 = d2 > bcast(radius2)     # [batch_size, width * height, 1]
+
+  # Output 0: doesintersect
+  # [batch_size, width * height, 1]
+  ifelsedoesintersect = ifelse(cond2, bcast(falses), bcast(trues))
+  ifelse2 = ifelse(cond1, bcast(falses), ifelsedoesintersect)
+  ifelse2 ⥅ doesintersect
+
+  # Output 1: t0
+  r2d2 = bcast(radius2) - d2  # [batch_size, width * height, 1]
+  thc = sqrt(r2d2)                # [batch_size, width * height, 1]
+  t0 = tca - thc                  # [batch_size, width * height, 1]
+  # [batch_size, width * height, 1]
+  t0out = ifelse(cond1, bcast(zerosscalar), ifelse(cond2, bcast(zerosscalar), t0))
+
+  # Output 2: t1
+  t1 = tca + thc                  # [batch_size, width * height, 1]
+  t1out = ifelse(cond1, bcast(zerosscalar), ifelse(cond2, bcast(zerosscalar), t1))
+  t0out ⥅ t0◂
+  t1out ⥅ t1◂
+  @assert is_wired_ok(rayint)
+  rayint
+end
+
+
 function leastpositive(shape, xs...)
   lp = first(xs)
   for x in xs[2:end]
@@ -94,7 +138,6 @@ function test_raytrace2()
   vec3size = (batch_size, width * height, 3)
   scalarsize = (batch_size, width * height, 1) # (batch_size, w * h)
   outimgsz = (batch_size, width * height, 1)
-
   rsarr = rayintersect_arr(vec3size, scalarsize)
   vec3nms = [:rdir, :rorig, :scenter]
   vec3⬨s = [⬨(rsarr, nm) for nm in vec3nms]
@@ -106,10 +149,24 @@ function test_raytrace2()
   rsarr, d1, d2
 end
 
-function test_raytrace()
-  width = 6
-  height = 4
-  batch_size = 2
+function namesz(arr::Arrow, dct::Dict{Symbol, Size})
+  Dict{SubPort, AbValues}(⬨(arr, k) => AbValues(:size => v) for (k,v) in dct)
+end
+
+function test_sizes(batch_size=2, width=5, height=5)
+  rsarr = rayintersect_arr_bcast()
+  szs = Dict(:sradius => Size([batch_size, 1]),
+             :scenter => Size([batch_size, 3]),
+             :rdir => Size([batch_size, width * height, 3]),
+             :rorig => Size([batch_size, width * height, 3]),
+             :doesintersect => Size([batch_size, width * height, 1]),
+             :t0 => Size([batch_size, width * height, 1]),
+             :t1 => Size([batch_size, width * height, 1]))
+  rsarr = rayintersect_arr_bcast()
+  rsarr, traceprop!(rsarr, namesz(rsarr, szs))
+end
+
+function test_rayintersect(width = 6, height = 4, batch_size = 2)
   vec3size = (batch_size, width * height, 3)
   scalarsize = (batch_size, width * height, 1) # (batch_size, w * h)
   outimgsz = (batch_size, width * height, 1)
@@ -123,7 +180,9 @@ function test_raytrace()
   d1 = Dict{SubPort, Arrows.AbValues}(vec3▹ => Arrows.AbValues(:size => Size(vec3size)) for vec3▹ in vec3⬨s)
   d2 = Dict{SubPort, Arrows.AbValues}(scalar⬨ => Arrows.AbValues(:size => Size(scalarsize)) for scalar⬨ in scalar⬨s)
   invert(rsarr, inv, merge(d1, d2))
+end
 
+function test_trc()
   rsarr(rand(vec3size...), rand(vec3size...), rand(vec3size...), 1.0)
   # TensorFlowTarget.Graph(rsarr)
   nspheres = 3
