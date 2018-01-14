bc180bd91cd1229f0863d606737e81c50f0a9705
Zenna Tavares <zennatavares@gmail.com>
2018-01-14 17:30:38 -0500

    native raytracer runs

--- src/raytrace/raytrace.jl
+++ src/raytrace/raytrace.jl
@@ -41,6 +41,8 @@ mix(a, b, mix::Real) = b * mix + a * (1 - mix)
 
 "norm(x)^2"
 dot_self(x) = dott(x, x)
+
+"Dot product (without BLAS etc for generality)"
 dott(xs, ys) = sum(xs .* ys)
 
 "normalized x: `x/norm(x)`"
@@ -68,18 +70,18 @@ function rayintersect(r::Ray, s::Sphere)::Intersection
   Intersection(radius2 - d2, t0, t1)
 end
 
-
 function trc(r::Ray, spheres::Vector{<:Sphere}, depth::Integer)
   tnear = Inf
   areintersections = false
 
   # Determine whether this ray hits any of the spheres, and if so, which one
   hit = false
-  for (i, sphere) in enumerate(spheres)
+  sphere = spheres[1] # 1 is arbitrary
+  for (i, target_sphere) in enumerate(spheres)
     t0 = Inf
     t1 = Inf
     r
-    inter = rayintersect(r, sphere)
+    inter = rayintersect(r, target_sphere)
     if inter.doesintersect > 0
       if inter.t0 < 0
         inter.t0 = t1
@@ -111,37 +113,37 @@ function trc(r::Ray, spheres::Vector{<:Sphere}, depth::Integer)
   bias = 1e-4;   # add some bias to the point from which we will be tracing
   inside = false
 
-  if dot_product(r.dir, nhit) > 0.0
+  if dott(r.dir, nhit) > 0.0
     nhit = -nhit
     inside = true
   end
 
-  if ((sphere->transparency > 0.0 || sphere->reflection > 0.0) && depth < 1)
-    minusrdir = r.dir * -1.0;
-    facingratio = dot_product(minusrdir, nhit)
+  if ((sphere.transparency > 0.0 || sphere.reflection > 0.0) && depth < 1)
+    minusrdir = r.dir * -1.0
+    facingratio = dott(minusrdir, nhit)
 
     # change the mix value to tweak the effect
-    fresneleffect = mix(pow((1.0 - facingratio),3), one, 0.1)
+    fresneleffect = mix((1.0 - facingratio)^3, 1.0, 0.1)
     # @show facingratio, fresneleffect, -r.dir, nhit
     # compute reflection direction (not need to normalize because all vectors
     # are already normalized)
-    refldir = r.dir - nhit * 2 * dot_product(r.dir, nhit)
+    refldir = r.dir - nhit * 2 * dott(r.dir, nhit)
     refldir = simplenormalize(refldir);
-    reflection = trace(Ray(phit + nhit * bias, refldir), spheres, depth + 1)
-    refraction = Vec3(zero)
+    reflection = trc(Ray(phit + nhit * bias, refldir), spheres, depth + 1)
+    refraction = Vec3([0.0, 0.0, 0.0])
 
     # the result is a mix of reflection and refraction (if the sphere is transparent)
     prod = reflection * fresneleffect
-    surface_color = multiply(prod, sphere.surface_color)
+    surface_color = map(*, prod, sphere.surface_color)
   else
-    for i = 1:len(spheres)
+    for i = 1:length(spheres)
       if spheres[i].emission_color[1] > 0.0
         # this is a light
         transmission = 1.0
         lightDirection = spheres[i].center - phit
         lightDirection = normalize(lightDirection)
 
-        for j = 1:len(spheres)
+        for j = 1:length(spheres)
           if (i != j)
             r2 = Ray(phit + nhit * bias, lightDirection)
             inter = rayintersect(r2, spheres[j])
@@ -150,8 +152,8 @@ function trc(r::Ray, spheres::Vector{<:Sphere}, depth::Integer)
             end
           end
         end
-        lhs = sphere.surface_color * transmission * rlu(dot_product(nhit, lightDirection))
-        surface_color += elt_multiply(lhs, spheres[i].emission_color)
+        lhs = sphere.surface_color * transmission * rlu(dott(nhit, lightDirection))
+        surface_color += map(*, lhs, spheres[i].emission_color)
       end
     end
   end
@@ -168,13 +170,15 @@ function render(spheres::Vector{<:Sphere},
   inv_height = 1 / height
   aspect_ratio = width / height
 
-  image = zeros(width, height)
+  image = zeros(width, height, 3)
   for y = 1:height, x = 1:width
     xx = (2 * ((x + 0.5) * inv_width) - 1) * angle * aspect_ratio
     yy = (1 - 2 * ((y + 0.5) * inv_height)) * angle
     minus1 = -1.0
     raydir = simplenormalize(Vec3([xx, yy, -1.0]))
-    image[x, y] = trc(Ray(Vec3([0.0, 0.0, 0.0]), raydir), spheres, 0)
+    pixel = trc(Ray(Vec3([0.0, 0.0, 0.0]), raydir), spheres, 0)
+    image[x, y, :] = pixel
+    # trc(Ray(Vec3([0.0, 0.0, 0.0]), raydir), spheres, 0)
     zero = 0.0
   end
   image
