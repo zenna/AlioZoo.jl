db24d3200a544428a584ec79c72c3e7339dc56b1
Javier Burroni <javier.burroni@gmail.com>
2018-01-15 23:28:29 -0500

    add test for `md2hash`

--- src/crypto/md2.jl
+++ src/crypto/md2.jl
@@ -14,7 +14,8 @@ function inverse_md2box(idx::Arrows.AbstractPort)
   res = Arrows.compose!(vcat(idx), Arrows.InverseMD2SBoxArrow())[1]
 end
 
-function compress(block, state, checksum)
+"function to compress the information of block into state"
+function compress(block, state, checksum, rounds=18)
   newstate = Array{Any, 1}(size(state)...)
   newchecksum = Array{Any, 1}(size(checksum)...)
   foreach(1:16) do  i
@@ -25,7 +26,6 @@ function compress(block, state, checksum)
   end
 
   t = Arrows.promote_constant(block[1] |> Arrows.anyparent, 0)
-  rounds = 18
   foreach(1:rounds) do i
     foreach(1:length(newstate)) do j
       t = newstate[j] = newstate[j] ⊻ md2box(t)
@@ -44,35 +44,20 @@ function compress(block, state, checksum)
 end
 
 
-
-function md2hash()
+"a hash compress the information of block and checksum in 16 bytes"
+function md2hash(rounds=18)
   carr = CompArrow(:compress, 16, 0);
   block = ▹(carr);
   state = [0 for i in 1:48];
   checksum = [0 for i in 1:16];
-  state, checksum = compress(block, state, checksum)
-  state, checksum = compress(checksum, state, checksum)
+  state, checksum = compress(block, state, checksum, rounds)
+  state, checksum = compress(checksum, state, checksum, rounds)
   foreach(link_to_parent!, state[1:16])
   Arrows.remove_dead_arrows!(carr)
   carr
 end
 
-carr = md2hash()
-println(carr(1:16...))
 
-if false
-  c = CompArrow(:compress, 16, 0);
-  block = in_sub_ports(c);
-  state = [0 for i in 1:48];
-  newstate = compress(block, state, 0)
-  println(c(1:16...))
-
-  inv_c = c |> Arrows.duplify |> Arrows.invert
-  md2_pgf = c |> pgf
-
-  (md2_pgf >> inv_c)(1:16...)
-  wirer, info = Arrows.solve(inv_c);
-end
 
 """
  An alternative to overcome the performance issues of julia is to use
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,2 @@
 include("test_voxel.jl")
+include("test_md2.jl")
