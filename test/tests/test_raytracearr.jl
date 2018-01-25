b1d1b630e8a919765e66435b9fe8b11f834508a5
Zenna Tavares <zennatavares@gmail.com>
2018-01-24 21:01:02 -0500

    training, finally

--- test/tests/test_raytracearr.jl
+++ test/tests/test_raytracearr.jl
@@ -41,3 +41,14 @@ function test_trc_invert(nspheres=1, batch_size=2, width=5, height=5)
 end
 
 test_trc_invert()
+
+function test_constraints(batch_size=2, width=5, height=5)
+  nmabv = AlioZoo.rtabv(batch_size, width, height)
+  rsarr = rayintersect_arr(batch_size, width, height)
+  invarr = invert(wrap(rsarr), inv, nmabv)
+  info = Arrows.ConstraintInfo()
+  Arrows.symbol_in_ports!(invarr, info, nmabv)
+  interpret(Arrows.sym_interpret, invarr, info.inp)
+end
+
+constraints = test_constraints()
